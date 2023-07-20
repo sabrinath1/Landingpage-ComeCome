cb857ca4c1ea26572e4e66afacd0230f5d68e424
Sabrina <sabrinath1@hotmail.com>
2023-07-20 16:53:02 -0300

    img banner

---
+++ components/molecules/Banner.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+const props = defineProps<{
+  imgBanner: string;
+}>();
+</script>
+<template>
+  <div
+    :style="{ backgroundImage: `url(${props.imgBanner})` }"
+    class="mb-20 bg-center w-full h-auto -mt-10 lg:mt-4"
+  >
+    <div
+      class="text-center lg:text-end text-neutral-content h-[30rem] w-full px-20"
+    >
+      <div class="flex-col pt-10">
+        <h1
+          class="mb-5 text-4xl lg: text-6xl font-bold pt-24 lg:pt-20 text-star items-center text-white"
+        >
+          Solução Completa Alimentação Industrial
+        </h1>
+      </div>
+    </div>
+  </div>
+</template>
--- components/organisms/BannerMain.vue
+++ components/organisms/BannerMain.vue
@@ -1,5 +1,10 @@
 <script setup lang="ts"></script>
 <template>
-  <MoleculesBannerDesktop imgDesktop="img/banner-desktop.png" />
-  <MoleculesBannerMobile imgMobile="img/banner-mobile.png" />
+  <MoleculesBanner
+    imgBanner="img/banner-desktop.png"
+    class="hidden md:block xl:hidden"
+  />
+  <!-- <MoleculesBanner imgBanner="img/banner-mobile.png" class="block md:hidden" /> -->
+  <MoleculesBanner imgBanner="img/banner-tablet.png" class="block md:hidden" />
+  <MoleculesBanner imgBanner="img/banner-tv.png" class="hidden xl:block" />
 </template>
