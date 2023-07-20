57aecf07d6aad6657b17e482068df26e7ad86749
Sabrina <sabrinath1@hotmail.com>
2023-07-20 17:02:09 -0300

    layoult title version mobile

--- components/molecules/Banner.vue
+++ components/molecules/Banner.vue
@@ -6,16 +6,16 @@ const props = defineProps<{
 <template>
   <div
     :style="{ backgroundImage: `url(${props.imgBanner})` }"
-    class="mb-20 bg-center w-full h-auto -mt-10 lg:mt-4"
+    class="mb-20 bg-center w-full h-auto -mt-32 lg:mt-4"
   >
     <div
-      class="text-center lg:text-end text-neutral-content h-[30rem] w-full px-20"
+      class="text-center lg:text-end text-neutral-content h-[30rem] w-full lg:px-20"
     >
-      <div class="flex-col pt-10">
+      <div class="flex-col pt-10 w-2/3 ml-32 md:ml-0">
         <h1
-          class="mb-5 text-4xl lg: text-6xl font-bold pt-24 lg:pt-20 text-star items-center text-white"
+          class="mb-5 text-4xl lg:text-6xl font-bold pt-24 mt-20 ml-4 lg:pt-20 text-center items-center text-white"
         >
-          Solução Completa Alimentação Industrial
+          Solução Completa em Alimentação Industrial
         </h1>
       </div>
     </div>
